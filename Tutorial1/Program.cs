fe94692b25127a8723152fdc063e2c6b01c6a381
MayHsuThwe <zinmaythwe12@gmail.com>
2022-09-28 19:40:20 +0630

    Add project files.

---
+++ Tutorial1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class StarProgram
+{
+    public static void Main()
+    {
+        Console.WriteLine("Output");        
+
+        for (int i = 1; i <= 6; i++)
+        {
+            for (int j = 1; j <= (6 - i); j++)
+            {
+                Console.Write(" ");
+            }
+           
+            for (int k = 1; k < i * 2; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 6 - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= (6 - i); j++)
+            {
+                Console.Write(" ");
+            }
+            
+            for (int k = 1; k < i * 2; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Tutorial2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public class CenturyAndLeap
+{
+    public static void GetCenturyAndCheckLeapYear(int year)
+    {
+        if (year <= 0)
+            Console.WriteLine("0 And Negative Not Allow");
+
+        else if (year <= 100)
+        {
+            int leapYear = year;
+            leapYear = Convert.ToInt32((((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? " 1 " : " -1 ");
+            Console.WriteLine("1st Century" + "," + leapYear );
+        }             
+
+        else if (year % 100 == 0)
+        {
+            int leapYear =year;
+            leapYear = Convert.ToInt32((((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? " 1 " : " -1 ");
+            Console.WriteLine(year / 100 + "," + leapYear);
+        }
+
+        else
+        {
+            int leapYear = year;
+            leapYear = Convert.ToInt32((((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? " 1 " : " -1 ");
+            Console.WriteLine(year / 100 + 1 + "," + leapYear);
+        }            
+    }    
+
+    public static void Main()
+    {        
+
+        Console.WriteLine("Example");
+        Console.Write("Input Year : ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        GetCenturyAndCheckLeapYear(year);
+    }
+}
+ 
